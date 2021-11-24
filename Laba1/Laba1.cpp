1c2f9a0f0b4d25f01d40e92eb831b4be2e01aa23
Illya <78978625+Transportnick@users.noreply.github.com>
2021-11-25 00:53:42 +0200

    Update Laba1.cpp
    ADD evklid

--- Laba1/Laba1.cpp
+++ Laba1/Laba1.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <string.h>
 #include <algorithm>
+#include <ctime>
 using namespace std;
 string HEXINBIT(string a)
 {
@@ -253,6 +254,10 @@ string LongMul(string n1, string n2)
 			c = LongAdd(temp, c);
 		}
 	}
+	while (c[0] == '0' && c.length() > 1)
+	{
+		c.erase(0, 1);
+	}
 	return c;
 }
 int LongCmp(string n1, string n2)
@@ -398,6 +403,14 @@ string GCD(string a, string b)
 	string d = "1";
 	string two = "10";
 	string a1;
+	while (a[0] == '0' && a.length() > 1)
+	{
+		a.erase(0, 1);
+	}
+	while (b[0] == '0' && b.length() > 1)
+	{
+		b.erase(0, 1);
+	}
 	while (a[a.length() - 1] == '0' && b[b.length() - 1] == '0')
 	{
 		a = LongDiv(a, two);
@@ -433,11 +446,77 @@ string GCD(string a, string b)
 	d = LongMul(a, d);
 	return d;
 }
-string GCK(string a, string b)
+string GCK(string a, string b, string i)
 {
 	string d = LongMul(a, b);
-	d = LongDiv(d, GCD(a, b));
-	return d;
+	while (i[0] == '0' && i.length() > 1)
+	{
+		i.erase(0, 1);
+	}
+	if (i == "1")
+	{
+		return d;
+	}
+	else
+	{
+		while (d[0] == '0' && d.length() > 1)
+		{
+			d.erase(0, 1);
+		}
+		d = LongDiv(d, i);
+		return d;
+	}
+}
+
+string KLD(string a, int l)
+{
+	if (a.length() < l) l = a.length();
+	a.resize(a.length() - l);
+	return a;
+}
+string Barret(string x, string n, string y)
+{
+	int k = n.length();
+	string q = KLD(x, k - 1);
+	q = LongMul(y, q);
+	q = KLD(q, k + 1);
+	q = LongMul(q, n);
+	string r = LongSub(x, q);
+	while (LongCmp(r, n) == 1)
+	{
+		r = LongSub(r, n);
+	}
+
+	while (r[0] == '0' && r.length() > 1)
+	{
+		r.erase(0, 1);
+	}
+	if (LongCmp(r, n) == 0)
+	{
+		r = "0";
+	}
+	return r;
+}
+string BarretLongPower(string a, string b, string n)
+{
+	string c = "1";
+	string y = LongShiftDigitsToHigh("1", 2 * n.length());
+	y = LongDiv(y, n);
+	while (y[0] == '0' && y.length() > 1)
+	{
+		y.erase(0, 1);
+	}
+	reverse(b.begin(), b.end());
+	for (int i = 0; i < b.length(); i++)
+	{
+		cout << i << endl;
+		if (b[i] == '1')
+		{
+			c = Barret(LongMul(c, a), n, y);
+		}
+		a = Barret(LongMul(a, a), n, y);
+	}
+	return c;
 }
 void FSTEST()
 {
@@ -469,48 +548,87 @@ void FSTEST()
 	string an = LongMul(n, a);
 	an = BININHEX(an);
 	cout << "na = " << an << endl;
+
 }
-string KLD(string a, int l)
+void FSTEST2()
 {
-	if (a.length() < l) l = a.length();
-	a.resize(a.length() - l);
-	return a;
-}
-string Barret(string x, string n, string y)
-{
-	int k = n.length();
-	string q = KLD(x, k - 1);
-	q = LongMul(y, q);
-	q = KLD(q, k + 1);
-	string r = LongSub(x, LongMul(q, n));
-	while (LongCmp(r, n) == 1)
+	string a, b, c, mod;
+	getline(cin, a);
+	getline(cin, b);
+	getline(cin, c);
+	getline(cin, mod);
+	a = HEXINBIT(a);
+	b = HEXINBIT(b);
+	c = HEXINBIT(c);
+	mod = HEXINBIT(mod);
+	string one = "1";
+	one = LongShiftDigitsToHigh(one, 2 * mod.length());
+	string y = LongDiv(one, mod);
+	string abc = LongMul(LongAdd(a, b), c);
+	abc = Barret(abc, mod, y);
+	abc = BININHEX(abc);
+	cout << "(a+b)c mod n = " << abc << endl;
+	string cab = LongMul(c, LongAdd(a, b));
+	cab = Barret(cab, mod, y);
+	cab = BININHEX(cab);
+	cout << "c(a+b) mod n = " << cab << endl;
+	string cacb = LongAdd(LongMul(a, c), LongMul(b, c));
+	cacb = Barret(cacb, mod, y);
+	cacb = BININHEX(cacb);
+	cout << "ca+cb modn = " << cab << endl;
+	string n = "C8";
+	string na = a;
+	n = HEXINBIT(n);
+	for (int i = 0; i < 199; i++)
 	{
-		r = LongSub(r, n);
+		na = LongAdd(na, a);
 	}
-	while (r[0] == '0' && r.length() > 1)
-	{
-		r.erase(0, 1);
-	}
-	return r;
+	na = Barret(na, mod, y);
+	na = BININHEX(na);
+	cout << "a+a+a+a+... mod n = " << na << endl;
+	string an = LongMul(n, a);
+	an = Barret(an, mod, y);
+	an = BININHEX(an);
+	cout << "ma modn = " << an << endl;
+
 }
-string BarretLongPower(string a, string b, string n)
+string Evklid(string a, string b)
 {
-	string c = "1";
-	string y = LongShiftDigitsToHigh("1", 2 * n.length());
-	y = LongDiv(y, n);
-	for (int i = 0; i < b.length() - 1; i++)
+	string r1 = a;
+	string r2 = b;
+	while (r1[0] == '0' && r1.length() > 1)
 	{
-		if (b[i] == '1')
+		r1.erase(0, 1);
+	}
+	while (r2[0] == '0' && r2.length() > 1)
+	{
+		r2.erase(0, 1);
+	}
+	while (r2 != "0")
+	{
+		r1 = LongMod(r1, r2);
+		if (r1 == "0")
 		{
-			c = Barret(LongMul(c, a), n, y);
+			r1 = r2;
+			break;
 		}
-		c = Barret(LongMul(a, a), n, y);
+		r2 = LongMod(r2, r1);
+		while (r1[0] == '0' && r1.length() > 1)
+		{
+			r1.erase(0, 1);
+		}
+		while (r2[0] == '0' && r2.length() > 1)
+		{
+			r2.erase(0, 1);
+		}
 	}
-	return c;
+	return r1;
 }
 
 int main()
 {
+	/*cout << "It`s a test 2:" << endl;
+	FSTEST2();*/
 	string number1, number2, number3;
 	getline(cin, number1);
 	getline(cin, number2);
@@ -552,16 +670,12 @@ int main()
 	cout << BININHEX(g) << endl;
 	string h = LongMod(e, num2);
 	cout << "A Mod B = " << endl << BININHEX(h) << endl;*/
-	string i = GCD(num1, num2);
-	cout << "GCD(A, B) = " << endl << BININHEX(i) << endl;
-	string l = GCK(num1, num2);
-	cout << "GCK(A, B) = " << endl << BININHEX(l) << endl;
 	getline(cin, number3);
 	string num3 = HEXINBIT(number3);
 	cout << num3 << endl;
-	int step = pow(2, 2 * num3.length());
-	string st = TENINDIT(step);
-	string y = LongMod(st, num3);
+	string one = "1";
+	one = LongShiftDigitsToHigh(one, 2 * num3.length());
+	string y = LongDiv(one, num3);
 	cout << "Modular operation :" << endl;
 	string o1 = Barret(LongAdd(num1, num2), num3, y);
 	cout << "A+B mod n = " << BININHEX(o1) << endl;
@@ -570,6 +684,24 @@ int main()
 	string o3 = Barret(LongMul(num1, num2), num3, y);
 	cout << "A*B mod n = " << BININHEX(o3) << endl;
 	string o4 = BarretLongPower(num1, num2, num3);
-	cout << "A*B mod n = " << BININHEX(o4) << endl;
+	cout << "A^B mod n = " << BININHEX(o4) << endl;
+	unsigned int start_time = clock();
+	string evklid = Evklid(num1, num2);
+	unsigned int end_time = clock();
+	unsigned int search_time = end_time - start_time;
+	cout << "GCDEVKLID(A, B) = " << endl << BININHEX(evklid) << endl << "Time: " << search_time << endl;
+	string evkl = GCK(num1, num2, evklid);
+	end_time = clock();
+	search_time = end_time - start_time;
+	cout << "GCKEvklid(A, B) = " << endl << BININHEX(evkl) << endl << "Time: " << search_time << endl;
+	start_time = clock();
+	string i = GCD(num1, num2);
+	end_time = clock();
+	search_time = end_time - start_time;
+	cout << "GCD(A, B) = " << endl << BININHEX(i) << endl << "Time: " << search_time << endl;
+	string l = GCK(num1, num2, i);
+	end_time = clock();
+	search_time = end_time - start_time;
+	cout << "GCK(A, B) = " << endl << BININHEX(l) << endl << "Time: " << search_time << endl;
 	return 0;
 }
